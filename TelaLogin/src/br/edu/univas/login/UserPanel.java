50cbdd5fd12aead6a830d9ac8f1b81d9f4057706
Marcio Azevedo <mecva.mecva@gmail.com>
2017-09-27 20:18:20 -0300

    Usando GridBagLayout e adicionando modelo UML.

--- TelaLogin/src/br/edu/univas/login/UserPanel.java
+++ TelaLogin/src/br/edu/univas/login/UserPanel.java
@@ -1,6 +1,8 @@
 package br.edu.univas.login;
 
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,24 +19,29 @@ public class UserPanel extends JPanel {
 	private JLabel passwordLabel;
 	private JPasswordField passwordField;
 
+	private GridBagConstraints nameLabelConstraints;
+	private GridBagConstraints nameTextFieldConstraints;
+	private GridBagConstraints passwordLabelConstraints;
+	private GridBagConstraints passwordFieldConstraints;
+
 	public UserPanel() {
 		initialize();
 	}
 
 	private void initialize() {
 		setBorder(new EmptyBorder(10, 10, 10, 10));
-		setLayout(new GridLayout(2, 2, 5, 5));
+		setLayout(new GridBagLayout());
 
-		add(getNameLabel());
-		add(getNameTextField());
-		add(getPasswordLabel());
-		add(getPasswordField());
+		add(getNameLabel(), getNameLabelConstraints());
+		add(getNameTextField(), getNameTextFieldConstraints());
+		add(getPasswordLabel(), getPasswordLabelConstraints());
+		add(getPasswordField(), getPasswordFieldConstraints());
 	}
 
 	private JLabel getNameLabel() {
 		if (nameLabel == null) {
 			nameLabel = new JLabel();
-			nameLabel.setText("Nome:");
+			nameLabel.setText("Nome do usuário:");
 		}
 		return nameLabel;
 	}
@@ -59,6 +66,50 @@ public class UserPanel extends JPanel {
 			passwordField = new JPasswordField();
 		}
 		return passwordField;
+	}
+
+	private GridBagConstraints getNameLabelConstraints() {
+		if (nameLabelConstraints == null) {
+			nameLabelConstraints = new GridBagConstraints();
+			nameLabelConstraints.gridx = 0;
+			nameLabelConstraints.gridy = 0;
+			nameLabelConstraints.anchor = GridBagConstraints.LINE_START;
+			nameLabelConstraints.insets = new Insets(5, 5, 5, 5);
+		}
+		return nameLabelConstraints;
+	}
+
+	private GridBagConstraints getNameTextFieldConstraints() {
+		if (nameTextFieldConstraints == null) {
+			nameTextFieldConstraints = new GridBagConstraints();
+			nameTextFieldConstraints.gridx = 1;
+			nameTextFieldConstraints.gridy = 0;
+			nameTextFieldConstraints.ipadx = 100;
+			nameTextFieldConstraints.insets = new Insets(5, 5, 5, 5);
+		}
+		return nameTextFieldConstraints;
+	}
+
+	private GridBagConstraints getPasswordLabelConstraints() {
+		if (passwordLabelConstraints == null) {
+			passwordLabelConstraints = new GridBagConstraints();
+			passwordLabelConstraints.gridx = 0;
+			passwordLabelConstraints.gridy = 1;
+			passwordLabelConstraints.anchor = GridBagConstraints.LINE_START;
+			passwordLabelConstraints.insets = new Insets(5, 5, 5, 5);
+		}
+		return passwordLabelConstraints;
+	}
+
+	private GridBagConstraints getPasswordFieldConstraints() {
+		if (passwordFieldConstraints == null) {
+			passwordFieldConstraints = new GridBagConstraints();
+			passwordFieldConstraints.gridx = 1;
+			passwordFieldConstraints.gridy = 1;
+			passwordFieldConstraints.ipadx = 100;
+			passwordFieldConstraints.insets = new Insets(5, 5, 5, 5);
+		}
+		return passwordFieldConstraints;
 	}
 
 }
