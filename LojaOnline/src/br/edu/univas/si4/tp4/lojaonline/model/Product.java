d21c9bdc53c21b26cfe6e1da91d7eb47fadb0f35
Marcio Azevedo <mecva.mecva@gmail.com>
2017-10-11 20:40:08 -0300

    Implementação da lista de produtos no DAO.

--- LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/Product.java
+++ LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/Product.java
@@ -2,4 +2,33 @@ package br.edu.univas.si4.tp4.lojaonline.model;
 
 public class Product {
 
+	private String name;
+	private int quantity;
+
+	public Product(String name, int quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", quantity=" + quantity + "]";
+	}
+
 }
--- LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/ProductDAO.java
+++ LojaOnline/src/br/edu/univas/si4/tp4/lojaonline/model/ProductDAO.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 public class ProductDAO {
 
 	public ArrayList<Product> listByName(String name) {
-		// TODO: Implementar...
-		return null;
+		ArrayList<Product> result = new ArrayList<>();
+		result.add(new Product("Arroz", 10));
+		result.add(new Product("Feijão", 20));
+		result.add(new Product("Farinha", 15));
+		result.add(new Product("Macarrão", 5));
+		return result;
 	}
 
 }
